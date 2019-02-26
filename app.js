75007bf3508a26509dc79ed72f8ac419293bdea0
Travis <43782081+SovietSombrero@users.noreply.github.com>
2019-02-26 08:54:51 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+let students = [];
+
+fs.readFile('students.json', (err, data) => {
+    students = JSON.parse(data);
+})
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/',(req, res) => {
+    
+});
+
+app.post('/answer',(req, res) => {
+    let ans = req.body.answer;
+    for (let i = 0; i < students.length; i++) {
+        if (ans == students[i].Student_Number) {
+            res.send(students[i]);
+        }
+    }
+})
+
+app.listen(3000, () => {
+    console.log('THE CRUSADE HAS BEGUN! TO WAR, COMRADES!');
+});
