3ac9ef0e9271a89d60458b00c81e00c9eeb4008d
Travis <43782081+SovietSombrero@users.noreply.github.com>
2019-03-04 08:09:06 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let students = [];
 
 let events = [];
 
-
+var grade;
 fs.readFile('students.json', (err, data) => {
     students = JSON.parse(data);  
 })
@@ -18,8 +18,22 @@ fs.readFile('events.json', (err, data) => {
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/',(req, res) => {
-   
+app.post('/signup', (req,res) => {
+    let ans = req.body.options;
+    let choice;
+     for (i = 0; i < events.length; i++) {
+         if (events[i].Event == ans) {
+             choice = events[i];
+         }
+     }
+    let package = {choice,grade,events}
+    res.send(package);
+    
+})
+
+app.get('/join',(req, res) => {
+    res.send(events);
+    //if I have to, run through events array and send the object.
 });
 
 app.post('/answer',(req, res) => {
@@ -27,6 +41,7 @@ app.post('/answer',(req, res) => {
     for (let i = 0; i < students.length; i++) {
         if (ans == students[i].Student_Number) {
             res.send(students[i]);
+            grade = students[i].Grade_Level;
         }
     }
 })
